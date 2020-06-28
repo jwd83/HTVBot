70e2f6981687255693e04b76bb60942704806e1b
Jared <jared0x90@gmail.com>
2020-06-27 21:48:22 -0400

    Create HTVBot.py

---
+++ HTVBot.py
@@ -0,0 +1,13 @@
+# Twitch API
+
+import time
+import twitch
+
+helix = twitch.Helix('client-id')
+
+# Users
+while True:
+    print("Grabbing room list")
+    for user in helix.users(['username']):
+        print(user.display_name)
+
